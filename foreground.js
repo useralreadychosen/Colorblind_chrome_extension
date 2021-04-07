e6926c074999179df220de0d79540a6714a7f926
Bryceson Laing <Bryceson@Brycesons-MacBook-Pro.local>
2021-04-06 20:40:52 -0500

    Initial commit

---
+++ foreground.js
@@ -0,0 +1,18 @@
+// document.querySelector('img').classList.add('spinspinspin');
+// document.body.style.backgroundColor="red";
+
+// const first = document.createElement('button');
+// first.innerText = "SET DATA";
+// first.id = "first";
+
+// const second = document.createElement('button');
+// second.innerText = "SHOUTOUT TO BACKEND";
+// second.id = "second";
+
+// document.querySelector('body').appendChild(first);
+// document.querySelector('body').appendChild(second);
+
+// first.addEventListener('click', () => {
+//     chrome.storage.local.set({"password": "123"});
+//     console.log("I SET DATA");
+// });
